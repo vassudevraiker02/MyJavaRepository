d13dc5c3efe992bcaa80318e82bd7a1628a65efb
Vassudev Raiker <vassudev_raiker@persistent.co.in>
2018-07-26 15:38:07 +0530

    git checkout -b ideagit checkout -b idea added and date display

--- HelloWorldGit.java
+++ HelloWorldGit.java
@@ -2,14 +2,14 @@ class HelloWorld {
 
 static void displayDate(){
 
-           System.out.println("display today's date:-");
+           System.out.println("display today's date:-"+newDate());
 }
        
 
 public static void main(String[] args){
 
 
- System.out.println("Hello World.... Welcome to GIT");
+ System.out.println("Hello World...!!!! Welcome to GIT...!!!");
 
    HelloWorld obj=new HelloWorld();
     obj.displayDate();
